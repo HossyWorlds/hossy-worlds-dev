392cb0a9bec5d81a12044ee4de60253142183b72
HossyWorlds <hta43435@gmail.com>
2025-06-02 22:29:57 +0900

    fix: OutlinedCardのcss編

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <div>
       <main className="flex flex-col gap-6">
-      <div className="flex gap-6 w-full max-w-6xl mx-auto mb-6 mt-6">
-          <div className="flex-1"><OutlinedCard /></div>
-          <div className="flex-1"><OutlinedCard /></div>
-          <div className="flex-1"><OutlinedCard /></div>
+        <div className="flex flex-wrap md:flex-nowrap gap-6 w-full max-w-6xl mx-auto mb-6 mt-6">
+          <div className="w-full md:flex-1"><OutlinedCard /></div>
+          <div className="w-full md:flex-1"><OutlinedCard /></div>
+          <div className="w-full md:flex-1"><OutlinedCard /></div>
         </div>
       </main>
     </div>
